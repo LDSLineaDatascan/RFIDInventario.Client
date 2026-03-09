6e5082877aad08a3fb50156fd38be11cad51fc91
LineaDataScanAFP <tu.email@empresa.com>
2026-03-09 12:24:05 -0500

    Feat: Menu actualizado segun rol

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AuthApi } from '../../services/auth-api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
   templateUrl: './menu.html',
   styleUrls: ['./menu.css'],
-  imports: [RouterModule]
+  imports: [RouterModule, CommonModule]
 })
-export class MenuComponent {}
+export class MenuComponent {
+
+  constructor(private auth: AuthApi) {}
+
+  get loggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
+
+  get rol(): string | null {
+    const usuario = localStorage.getItem("usuarioSesion");
+    if (!usuario) return null;
+
+    return JSON.parse(usuario).rol;
+  }
+
+  get isAdmin(): boolean {
+    return this.rol === 'Admin';
+  }
+
+  get isUser(): boolean {
+    return this.rol === 'User';
+  }
+}
